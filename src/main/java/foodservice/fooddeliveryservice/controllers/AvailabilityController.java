3b25caed09c72077211a009e9f56e949da3c81b2
malviyk <kuldeep.malviy@ig.com>
2020-07-12 13:14:42 +0530

    update 1 : added logs

--- src/main/java/foodservice/fooddeliveryservice/controllers/AvailabilityController.java
+++ src/main/java/foodservice/fooddeliveryservice/controllers/AvailabilityController.java
@@ -4,12 +4,14 @@ package foodservice.fooddeliveryservice.controllers;
 import foodservice.fooddeliveryservice.models.FoodItem;
 import foodservice.fooddeliveryservice.services.AvailabilityService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class AvailabilityController {
@@ -18,10 +20,10 @@ public class AvailabilityController {
 
    @GetMapping("/restaurant/{restaurant}/area/{yourLocation}")
    public List<FoodItem> getAvailableFoodItem(@PathVariable String restaurant, @PathVariable String yourLocation) {
-
-      return availabilityService.getFoodItems(restaurant, yourLocation);
-
-
+      log.info("Received request to fetch food items, restaurant={} location={}", restaurant, yourLocation);
+      List<FoodItem> foodItems = availabilityService.getFoodItems(restaurant, yourLocation);
+      log.info("Returning food for restaurant={} location={}, foodItems={}", restaurant, yourLocation, foodItems);
+      return foodItems;
    }
 
 
--- src/main/java/foodservice/fooddeliveryservice/controllers/OderController.java
+++ src/main/java/foodservice/fooddeliveryservice/controllers/OderController.java
@@ -5,45 +5,54 @@ import foodservice.fooddeliveryservice.models.FoodItem;
 import foodservice.fooddeliveryservice.models.OrderSummary;
 import foodservice.fooddeliveryservice.services.OrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+
+@Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/order/customer")
 public class OderController {
 
    private final OrderService orderService;
 
-   @PostMapping("/order/customer/{customerNumber}")
+   @PostMapping("/{customerNumber}")
    public Integer createOrder(@PathVariable("customerNumber") Integer customerNumber, List<FoodItem> foodItems) {
-      return orderService.createOrder(customerNumber, foodItems);
+      log.info("Received request create new order for customer={}, foodItem={}", customerNumber, foodItems);
+      int orderId = orderService.createOrder(customerNumber, foodItems);
+      log.info("Order created with orderId={}, customer={}", orderId, customerNumber);
+      return orderId;
    }
 
-   @PutMapping("/order/customer/{customerNumber}/order/{orderId}")
+   @PutMapping("/{customerNumber}/order/{orderId}")
    public boolean updateOrder(@PathVariable Integer customerNumber, @PathVariable Integer orderId, List<FoodItem> foodItems) {
-      return orderService.updateOrder(customerNumber, orderId, foodItems);
+      log.info("Received request to update order for customer={},orderId={}, foodItem={}", customerNumber, orderId, foodItems);
+      boolean updateOrder = orderService.updateOrder(customerNumber, orderId, foodItems);
+      log.info("Order updated={} for customer={}, orderId={},", updateOrder, customerNumber, orderId);
+      return updateOrder;
    }
 
-   @DeleteMapping("/order/customer/{customerNumber}/order/{orderId}")
+   @DeleteMapping("/{customerNumber}/order/{orderId}")
    public void deleteOrder(@PathVariable Integer customerNumber, @PathVariable Integer orderId) {
+      log.info("Received request to delete order customer={}, orderId={}", orderId, customerNumber);
       orderService.delete(customerNumber, orderId);
    }
 
-   @GetMapping("/order/customer/{customerNumber}/order/{orderId}/summary")
+   @GetMapping("/{customerNumber}/order/{orderId}/summary")
    public OrderSummary getOrderSummary(@PathVariable Integer customerNumber, @PathVariable Integer orderId) {
-      return orderService.getOrderSummary(customerNumber, orderId);
-   }
-
-
-   @GetMapping("hi")
-   public String hi() {
-      return "Hr There!";
+      log.info("Received request to fetch Order details,customer={}, orderId={}", customerNumber, orderId);
+      OrderSummary orderSummary = orderService.getOrderSummary(customerNumber, orderId);
+      log.info("Received request to fetch Order details,customer={}, orderId={}", customerNumber, orderId);
+      return orderSummary;
    }
 
 }
